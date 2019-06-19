0cbcc6c7a1b567c2f0e6b9cf5fb9c4318a1479b7
Andrew Stanton <up683693@myport.ac.uk>
2019-06-19 16:24:50 +0100

    Added comments, moved downloads into a folder

--- getCourses.py
+++ getCourses.py
@@ -1,35 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Prepend to relative URLs
 baseUrl = "https://www.port.ac.uk"
+# The page that lists courses
+# TODO Pagination or pass params to grab all courses
 courseListUrl = "https://www.port.ac.uk/study/courses"
+# CSS selector to ge the hyperlinked headers
 courseLinksQuery = ".Content .o-Grid h2>a"
+# Store course info here
 courseList = []
 
-page = requests.get(courseListUrl)
+# Get and parse course listings
+courseListPageSource = requests.get(courseListUrl)
+parsedCourseList = BeautifulSoup(courseListPageSource.text, 'html.parser')
 
-soup = BeautifulSoup(page.text, 'html.parser')
-
-courses = soup.select(courseLinksQuery)
-
+# Get the name and URL for each course
+courses = parsedCourseList.select(courseLinksQuery)
 print("There are " + str(len(courses)) + " courses")
-
 for course in courses:
     currentCourseInfo = {}
-    name = course.text
-    print(name)
-    currentCourseInfo["name"] = name
+    currentCourseInfo["name"] = course.text
     currentCourseInfo["url"] = course['href']
     courseList.append(currentCourseInfo)
 
-for course in courseList[0:1]:
+# Get each course
+for course in courseList:
     fullURL = baseUrl + course["url"]
-    print(fullURL)
     coursePage = requests.get(fullURL)
     coursePageSource = BeautifulSoup(coursePage.text, 'html.parser')
-    prettySource = coursePageSource.prettify()
-    # print(prettySource)
-    fileName = course["name"] + ".html"
+    fileName = "downloads/" + course["name"] + ".html"
     print(fileName)
     with open(fileName, "w") as f:
-        f.write(prettySource)
+        f.write(coursePageSource.prettify())
